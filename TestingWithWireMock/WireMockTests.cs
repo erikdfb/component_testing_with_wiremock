272b317648dec321c348b7e35b16d823732efb64
Erik Barbosa <erik.barbosa@clear.bank>
2023-04-21 15:34:36 +0100

    Initial commit

---
+++ TestingWithWireMock/WireMockTests.cs
@@ -0,0 +1,87 @@
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+public class WireMockTests : IDisposable
+{
+    private readonly HttpClient _httpClient;
+    private readonly WireMockServer _wireMockServer;
+
+    public WireMockTests()
+    {
+        // Create a new instance of the WireMock server
+        _wireMockServer = WireMockServer.Start();
+
+        // Create a new HttpClient instance with the base URL of our WireMock server
+        _httpClient = new HttpClient { BaseAddress = new Uri(_wireMockServer.Url) };
+    }
+
+    [Fact]
+    public async Task Get_Endpoint_Returns_Success()
+    {
+        // Arrange
+        const string expectedResponse = "Hello, World!";
+
+        // Set up a WireMock stub for our API endpoint
+        _wireMockServer
+            .Given(Request.Create()
+                .WithPath("/api/hello")
+                .UsingGet())
+            .RespondWith(Response.Create()
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithBody(expectedResponse));
+
+        // Act (use a HttpClient which connects to the URL where WireMock.Net is running)
+        var response = await _httpClient.GetAsync("/api/hello");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var responseContent = await response.Content.ReadAsStringAsync();
+        Assert.Equal(expectedResponse, responseContent);
+    }
+
+    [Fact]
+    public async Task CreateNewUser_Returns_CreatedUser()
+    {
+        // Arrange
+        _wireMockServer
+            .Given(Request.Create()
+                .WithPath("/api/users")
+                .UsingPost()
+                .WithBody("{\"Name\":\"John Doe\",\"Email\":\"johndoe@example.com\"}"))
+            .RespondWith(Response.Create()
+                .WithStatusCode(201)
+                .WithBody("{\"Id\":1,\"Name\":\"John Doe\",\"Email\":\"johndoe@example.com\"}"));
+
+        var newUser = new
+        {
+            Name = "John Doe",
+            Email = "johndoe@example.com"
+        };
+
+        var json = JsonConvert.SerializeObject(newUser);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _httpClient.PostAsync("/api/users", content);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var createdUser = JsonConvert.DeserializeObject<User>(responseBody);
+
+        Assert.Equal(1, createdUser.Id);
+        Assert.Equal("John Doe", createdUser.Name);
+        Assert.Equal("johndoe@example.com", createdUser.Email);
+    }
+
+    public void Dispose()
+    {
+        // Stop the WireMock server
+        _wireMockServer.Stop();
+    }
+}
